136a4557b8def409f261c9b20ec9df354df3d1c7
Jackson Chen <jacksonchen171@gmail.com>
2020-03-26 22:26:13 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -5,9 +5,3 @@ const main = document.getElementById('main');
 main.addEventListener('click', function(event) {
   alert ('I was clicked!'0);
 });
-
-const input = document.querySelector('input');
-
-input.addEventListener('keydown', function(e) {
-  console.log(e.key);
-});
